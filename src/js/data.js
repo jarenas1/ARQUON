df6328c4520e5c6baadf5764211dbb77bc1c312d
jarenas1 <juanjoarenas1218gmail.com>
2024-06-30 10:01:23 -0500

    data-structure

---
+++ src/js/data.js
@@ -0,0 +1,89 @@
+const projects = [
+    {   type1:[
+        {
+        id: 1,
+        title: 'Project 1',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        img1: 'project1.jpg',
+        img2: 'project2.jpg',
+        img3: 'project3.jpg',
+        img4: 'project4.jpg',
+        img5: 'project5.jpg',
+        year: '2020',
+        typeOfProject: 'aaaaa'    
+        },
+
+        {  
+            id: 1,
+            title: 'Project 1',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            img1: 'project1.jpg',
+            img2: 'project2.jpg',
+            img3: 'project3.jpg',
+            img4: 'project4.jpg',
+            img5: 'project5.jpg',
+            year: '2020',
+            typeOfProject: 'aaaaa'
+        }
+
+        ],
+
+        type2:[
+        {
+            id: 1,
+            title: 'Project 1',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            img1: 'project1.jpg',
+            img2: 'project2.jpg',
+            img3: 'project3.jpg',
+            img4: 'project4.jpg',
+            img5: 'project5.jpg',
+            year: '2020',
+            typeOfProject: 'aaaaa'
+            },
+
+            {  
+                id: 1,
+                title: 'Project 1',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                img1: 'project1.jpg',
+                img2: 'project2.jpg',
+                img3: 'project3.jpg',
+                img4: 'project4.jpg',
+                img5: 'project5.jpg',
+                year: '2020',
+                typeOfProject: 'aaaaa'
+            }
+        ],
+
+        type3:[
+        {
+            id: 1,
+            title: 'Project 1',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            img1: 'project1.jpg',
+            img2: 'project2.jpg',
+            img3: 'project3.jpg',
+            img4: 'project4.jpg',
+            img5: 'project5.jpg',
+            year: '2020',
+            typeOfProject: 'aaaaa'
+            },
+
+            {  
+                id: 1,
+                title: 'Project 1',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                img1: 'project1.jpg',
+                img2: 'project2.jpg',
+                img3: 'project3.jpg',
+                img4: 'project4.jpg',
+                img5: 'project5.jpg',
+                year: '2020',
+                typeOfProject: 'aaaaa'
+            }
+        ]
+        
+        
+    }
+]
